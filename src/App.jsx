4d86f9501ab25a86088ad84dabbcaf1f03fb34f0
lakshraj nagle <lakshrajnagle.com>
2025-03-26 15:50:38 +0530

    create pages and  routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
+
+import {BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import SignUp from './pages/SignUp';
+import About from './pages/about'; // Renamed from "about" to "About" to match component name
+import Profile from './pages/Profile';
+
+ // Importing Home component
 export default function App() {
   return (
-    <h1 className=" text-blue-500">
+  <BrowserRouter >
       
-      Hello world!
-    </h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />  Renamed to match component name
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
